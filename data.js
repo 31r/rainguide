a95f1c52854791f2c9aa405100310ba5252722b5
31r <tseronnie@ymail.com>
2026-05-26 01:39:15 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,232 @@
+        // ==========================================
+        // 1. data.js
+        // ==========================================
+        const csvData = `Start_Node,End_Node,Weight,Type,Description
+
+// --- SAI YING PUN / HKU ---
+sai_ying_pun_platform,syp_concourse,2,fare_gate,Fare gates
+syp_concourse,eco_tree_hotel,1,internal,Exit A2 to Eco Tree Hotel
+hku_platform,hku_concourse,2,fare_gate,Fare gates
+hku_concourse,hku_campus,2,elevator,Exit A2 Elevators to HKU Main Campus
+hku_concourse,hku_campus_c,2,elevator,Exit C1 Elevators to HKU Centennial Campus
+hku_campus,hku_campus_c,2,internal,HKU Internal Walkway
+
+// --- CENTRAL & ADMIRALTY ---
+shun_tak_centre,west_spine_west,2,bridge,Bridge along Connaught Road Central
+infinitus_plaza,west_spine_west,1,bridge,Bridge over Connaught Road Central
+rumsey_carpark,west_spine_west,1,bridge,Bridge along Connaught Road Central
+harbour_bldg,west_spine_central,1,bridge,Bridge along Connaught Road Central
+two_chinachem,west_spine_east,1,bridge,Bridge along Connaught Road Central
+one_ifc,west_spine_east,3,bridge,Bridge along Connaught Road Central
+west_spine_west,west_spine_central,3,bridge,Bridge along Connaught Road Central
+west_spine_central,west_spine_east,2,bridge,Bridge along Connaught Road Central
+one_ifc,ifc_mall,1,internal,Internal IFC Connection
+two_ifc,ifc_mall,1,internal,Internal IFC Connection
+one_ifc,3_exchange_square,1,bridge,Bridge
+hang_seng_hq,one_ifc,2,bridge,Bridge over Connaught Road Central
+hang_seng_hq,3_exchange_square,3,bridge,Bridge over Connaught Road Central
+3_exchange_square,1&2_exchange_square,2,bridge,Bridge
+ifc_mall,1&2_exchange_square,2,bridge,Bridge
+yue_thai_bldg,sheung_wan_concourse_mid,2,tunnel,Sheung Wan Exit C
+kam_sang_bldg,sheung_wan_concourse_mid,2,tunnel,Sheung Wan Exit B
+tung_hip_bldg,sheung_wan_concourse_mid,2,tunnel,Sheung Wan Exit A1
+shun_tak_centre,sheung_wan_concourse_mid,3,tunnel,Sheung Wan Exit D
+infinitus_plaza,sheung_wan_concourse_east,2,tunnel,Sheung Wan Exit E5
+golden_centre,sheung_wan_concourse_east,2,tunnel,Sheung Wan Exit E1
+ifc_mall,central_pier_3,2,bridge,Bridge
+central_yards,central_pier_7,2,bridge,Bridge
+central_pier_3,central_pier_7,3,internal,Covered Walkway
+spine_north,central_yards,0.5,bridge,Bridge
+spine_north,ifc_mall,0.5,bridge,Bridge
+spine_north,spine_mid,1,bridge,Bridge
+spine_mid,gpo,0.5,bridge,Bridge
+spine_mid,1&2_exchange_square,0.5,bridge,Bridge
+spine_mid,jardine_house,0.5,bridge,Bridge
+spine_mid,spine_south,1,bridge,Bridge over Connaught Road Central
+spine_south,world_wide_house,1,bridge,Bridge
+world_wide_house,1&2_exchange_square,2,bridge,Bridge over Connaught Road Central
+spine_south,chater_house,0,bridge,Grand Trunk Terminus
+chater_house,alexandra_house,1,bridge,Bridge over Chater Road
+hang_seng_hq,central_market,1,bridge,Bridge over Des Voeux Road Central
+central_market,100qrc,2,bridge,Bridge over Queen's Road Central
+hang_seng_hq,nexxus_bldg,1,bridge,Bridge over Queen Victoria Street
+landmark_gloucester,alexandra_house,1,bridge,Bridge over Des Voeux Road Central
+landmark_mall,alexandra_house,1,bridge,Bridge over Des Voeux Road Central
+landmark_mall,bea_bldg,1,internal,Internal Landmark Connection
+landmark_mall,central_bldg,1,internal,Internal Landmark Connection
+central_bldg,central_tower,1,bridge,Bridge over Queen's Road Central
+central_tower,entertainment_bldg,1,bridge,Bridge over Wyndham Street
+landmark_mall,9qrc,3,bridge,Bridge over Ice House Street
+landmark_gloucester,landmark_mall,1,internal,Internal Landmark Connection
+landmark_edinburgh,landmark_mall,1,internal,Internal Landmark Connection
+landmark_york,landmark_mall,1,internal,Internal Landmark Connection
+alexandra_house,princes_bldg,1,bridge,Bridge over Ice House Street
+princes_bldg,mandarin_oriental,1,bridge,Bridge over Chater Road
+princes_bldg,9qrc,2,bridge,Bridge over Des Voeux Road Central
+princes_bldg,scb_bldg,2,bridge,Bridge over Des Voeux Road Central
+9qrc,scb_bldg,1,bridge,Short Bridge
+scb_bldg,hsbc_bldg,1,bridge,Short Bridge
+chater_garden,aia_central,3,bridge,Bridge over Chater Road
+chater_garden,the_henderson,3,bridge,Bridge over Murray Road
+the_henderson,aia_central,2,bridge,Bridge over Cotton Tree Drive
+the_henderson,cheung_kong_centre,3,bridge,Bridge over Queen's Road Central
+cheung_kong_centre,three_garden_road,2,bridge,Bridge over Garden Road
+three_garden_road,the_murray,1,bridge,Bridge
+the_henderson,central_east_node,1,bridge,Bridge
+bank_of_america,central_east_node,1,bridge,Bridge over Lambeth Walk
+fairmont_house,central_east_node,1,bridge,Bridge
+queensway_plaza,central_east_node,3,bridge,Bridge over Cotton Tree Drive
+queensway_plaza,admiralty_centre,1,bridge,Bridge
+queensway_plaza,far_east_finance,1,bridge,Bridge
+queensway_plaza,united_centre,1,internal,Internal Mall Walkway
+queensway_plaza,lippo_centre,1,bridge,Bridge
+admiralty_centre,central_gov_offices,3,bridge,Bridge over Harcourt Road
+citic_tower,admiralty_exit_e2,3,bridge,Bridge over Harcourt Road
+citic_tower,central_gov_offices,3,bridge,Bridge over Tim Mei Avenue
+united_centre,admiralty_exit_e2,3,bridge,Bridge to MTR
+high_court,lippo_centre,3,bridge,Bridge over Queensway
+high_court,queensway_gov_offices,1,bridge,Short Bridge
+one_ifc,hong_kong_concourse,1,escalator,Hong Kong Exit E
+two_ifc,hong_kong_concourse,1,escalator,Hong Kong Exit A
+ifc_mall,hong_kong_concourse,1,escalator,Hong Kong Exits A/E
+world_wide_house,central_concourse_west,2,escalator,Central Exits A/B
+chater_house,central_concourse_mid,2,escalator,Central Exit E
+alexandra_house,central_concourse_mid,2,escalator,Central Exit H
+landmark_mall,central_concourse_mid,2,escalator,Central Exit G
+chater_garden,central_concourse_east,2,escalator,Central Exit J2
+ccb_tower,central_concourse_east,2,escalator,Central Exit L
+queensway_plaza,admiralty_concourse,1,escalator,Admiralty Exits B/C
+admiralty_centre,admiralty_concourse,1,escalator,Admiralty Exit A
+admiralty_exit_e2,admiralty_concourse,3,escalator,Admiralty Exit E2 Escalator
+central_concourse_west,central_concourse_mid,2,tunnel,Central MTR Unpaid Link
+central_concourse_mid,central_concourse_east,5,tunnel,Central MTR Unpaid Link
+pacific_place_lg1,pacific_place_l2,4,internal,Internal escalators (L1 to L2)
+pacific_place_lg1,admiralty_concourse,2,tunnel,Admiralty Exit F
+pacific_place_lg1,three_pacific_place,2,tunnel,Tunnel
+pacific_place_l2,queensway_gov_offices,1,bridge,Short Bridge
+pacific_place_l2,united_centre,3,bridge,Bridge over Queensway
+pacific_place_l2,queensway_plaza,2,bridge,Bridge over Queensway
+pacific_place_l2,admiralty_exit_e2,3,bridge,Bridge connection to Admiralty Exit E2
+
+// --- CENTRAL TO HONG KONG PAID TUNNEL ---
+central_platform_island,hong_kong_platform,3,paid_tunnel,Paid tunnel with moving walkways
+hong_kong_platform,hong_kong_concourse,1,fare_gate,Fare gates to HK Station Exits
+
+// --- SHEUNG WAN TO ADMIRALTY MTR PLATFORMS & FARE GATES ---
+sheung_wan_platform,sheung_wan_concourse_mid,1,fare_gate,Fare gates to Exits A-D
+sheung_wan_platform,sheung_wan_concourse_east,2,fare_gate,Fare gates to Exit E
+central_platform_island,central_concourse_west,2,fare_gate,Fare gates to Exits A-E
+central_platform_island,central_concourse_east,3,fare_gate,Fare gates to Exits J-L
+admiralty_platform,admiralty_concourse,2,fare_gate,Fare gates to Admiralty Exits
+
+// --- WAN CHAI HUB ---
+wan_chai_platform,wan_chai_concourse,1,fare_gate,Fare gates
+wan_chai_concourse,china_overseas_bldg,1,internal,Exit C
+wan_chai_concourse,immigration_tower,2,bridge,Exit A5 Footbridge
+immigration_tower,central_plaza,1,bridge,Bridge to Central Plaza
+immigration_tower,revenue_tower,1,escalator,Covered Walkway
+central_plaza,hkcec,1,bridge,Bridge to HKCEC
+hkcec,great_eagle_ctr,2,bridge,Bridge over Fleming Road
+great_eagle_ctr,harbour_ctr,1,internal,Podium
+great_eagle_ctr,st_regis,2,bridge,Bridge over Fleming Road
+harbour_ctr,wcn_junction,0.5,bridge,Bridge
+wcsp,wcn_junction,0.5,bridge,Bridge
+causeway_ctr,wcn_junction,1.5,bridge,Bridge
+causeway_ctr,shk_ctr,2,escalator,Covered Walkway across Harbour Drive
+shk_ctr,capital_ctr,3,bridge,Bridge over Gloucester Road
+central_plaza,st_regis,4,bridge,Bridge over Harbour Road
+st_regis,cr_bldg,1,internal,Podium
+cr_bldg,causeway_ctr,2,internal,Podium
+wan_chai_concourse,southorn_stadium,2,internal,Exit B2 to Southorn Stadium
+wan_chai_concourse,lee_tung_avenue,2,tunnel,Exit D to Lee Tung Avenue
+exhibition_centre_a3,wcn_junction,0.5,bridge,Escalator
+exhibition_centre_b3,great_eagle_ctr,1,bridge,Escalator
+exhibition_centre_b3,hkcec,2,bridge,Escalator and Bridge over Fleming Road
+
+// --- CAUSEWAY BAY HUB ---
+causeway_bay_platform,causeway_bay_concourse_a,1,fare_gate,Fare gates to Exit A (Times Square)
+causeway_bay_platform,causeway_bay_concourse_b_c,1,fare_gate,Fare gates to Exits B-C
+causeway_bay_platform,causeway_bay_concourse_d_f,1,fare_gate,Fare gates to Exits D-F
+causeway_bay_concourse_a,times_square,1,internal,Internal Walkway to Times Square
+causeway_bay_concourse_b_c,cwb_plaza,1,internal,Exit B to Causeway Bay Plaza 1
+causeway_bay_concourse_d_f,hysan_place,1,internal,Exit F2 to Hysan Place
+causeway_bay_concourse_d_f,sogo_cwb,1,internal,Exit D2 to SOGO Basement
+
+// --- FORTRESS HILL HUB ---
+fortress_hill_platform,fortress_hill_concourse,1,fare_gate,Fare gates to Exit B
+fortress_hill_concourse,fortress_metro_tower,1,escalator,Exit B to Fortress Metro Tower
+fortress_metro_tower,fortress_hill_t_junction,2,bridge,Bridge
+electric_road_msb,fortress_hill_t_junction,1,bridge,Bridge
+harbour_glory,fortress_hill_t_junction,3,bridge,Bridge
+
+// --- NORTH POINT HUB ---
+north_point_platform,north_point_concourse,1,fare_gate,Fare gates to Exits A1-A4
+north_point_platform,north_point_concourse,1,fare_gate,Fare gates to Exit B
+north_point_concourse,odeon_building,1,escalator,Exits A1-A3 to Odeon Building
+north_point_concourse,china_united_centre,1,escalator,Exit A4 to China United Centre
+north_point_concourse,roca_centre,1,escalator,Exit B to Roca Centre
+
+// --- TAI KOO HUB ---
+tai_koo_platform,tai_koo_concourse,1,fare_gate,Fare gates
+tai_koo_concourse,cityplaza_gf,2,internal,Exit D2 to Cityplaza G/F
+tai_koo_concourse,cityplaza_2f,2,internal,Exit E1 to Cityplaza 2/F
+cityplaza_gf,cityplaza_1f,2,internal,Internal Cityplaza Escalators
+cityplaza_1f,cityplaza_2f,1,internal,Internal Cityplaza Escalators
+cityplaza_gf,horizon_gardens,1,internal,Covered Walkway
+cityplaza_1f,kam_din_terrace,1,bridge,Bridge over Tai Mou Avenue
+cityplaza_1f,sing_fai_terrace,1,bridge,Bridge over Tai Fung Avenue
+cityplaza_2f,east_hk,1,bridge,Bridge
+cityplaza_2f,kao_shan_terrace,1,bridge,Bridge over Tai Fung Avenue
+cityplaza_2f,12_taikoo,1,bridge,Bridge over Taikoo Wan Road
+cityplaza_2f,14_taikoo,1,bridge,Bridge over Taikoo Wan Road
+cityplaza_2f,1111_kings,3,internal,Internal Cityplaza Escalators
+tai_koo_concourse,kornhill_plaza_n,2,internal,Exit A2 to Kornhill Plaza (North)
+tai_koo_concourse,kornhill_plaza_s,2,internal,Exit C to Kornhill Plaza (South)
+kornhill_plaza_n,kornhill_plaza_s,1,bridge,Bridge over Kornhill Road
+kornhill_plaza_s,kornhill_a-h,4,escalator,Escalator and Bridge
+kornhill_plaza_s,kornhill_j-m,3,escalator,Escalator and Bridge
+kornhill_a-h,kornhill_j-m,2,staircase,Staircase
+
+// --- SAI WAN HO HUB ---
+sai_wan_ho_platform,sai_wan_ho_concourse,1,fare_gate,Fare gates to Exit A
+sai_wan_ho_concourse,felicity_garden,1,escalator,Exit A to Felicity Garden
+
+// --- HENG FA CHUEN HUB ---
+heng_fa_chuen_platform,heng_fa_chuen_concourse,1,fare_gate,Fare gates
+heng_fa_chuen_concourse,paradise_mall,1,internal,MTR direct connection to Mall
+
+// --- CHAI WAN HUB ---
+chai_wan_platform,chai_wan_concourse,1,fare_gate,Fare gates to Exit A
+chai_wan_concourse,new_jade_garden,1,internal,Exit A to New Jade Garden
+hing_wah_1_estate,new_jade_garden,2,bridge,Bridge
+new_jade_garden,chai_wan_y_junction,1,bridge,Bridge
+wan_tsui_estate,chai_wan_y_junction,2,bridge,Bridge
+youth_square,chai_wan_y_junction,1,bridge,Bridge
+
+// --- THE ISLAND LINE (TRAIN TRACKS) ---
+kennedy_town_platform,hku_platform,2,paid_train,Island Line (MTR)
+hku_platform,sai_ying_pun_platform,2,paid_train,Island Line (MTR)
+sai_ying_pun_platform,sheung_wan_platform,2,paid_train,Island Line (MTR)
+central_platform_island,sheung_wan_platform,2,paid_train,Island Line (MTR)
+admiralty_platform,central_platform_island,2,paid_train,Island Line (MTR)
+admiralty_platform,wan_chai_platform,2,paid_train,Island Line (MTR)
+wan_chai_platform,causeway_bay_platform,2,paid_train,Island Line (MTR)
+causeway_bay_platform,tin_hau_platform,2,paid_train,Island Line (MTR)
+tin_hau_platform,fortress_hill_platform,2,paid_train,Island Line (MTR)
+fortress_hill_platform,north_point_platform,2,paid_train,Island Line (MTR)
+north_point_platform,quarry_bay_platform,2,paid_train,Island Line (MTR)
+quarry_bay_platform,tai_koo_platform,2,paid_train,Island Line (MTR)
+tai_koo_platform,sai_wan_ho_platform,2,paid_train,Island Line (MTR)
+sai_wan_ho_platform,shau_kei_wan_platform,2,paid_train,Island Line (MTR)
+shau_kei_wan_platform,heng_fa_chuen_platform,2,paid_train,Island Line (MTR)
+heng_fa_chuen_platform,chai_wan_platform,2,paid_train,Island Line (MTR)
+
+// --- ADMIRALTY INTERCHANGE & EAST RAIL LINE ---
+admiralty_platform,admiralty_platform_erl,3,paid_transfer,Escalators to East Rail Line platforms
+admiralty_platform_erl,exhibition_centre_platform,2,paid_train,East Rail Line (MTR)
+
+// --- EXHIBITION CENTRE HUB ---
+exhibition_centre_platform,exhibition_centre_concourse,1,fare_gate,Fare gates
+exhibition_centre_concourse,exhibition_centre_a3,1,internal,Exit A3
+exhibition_centre_concourse,exhibition_centre_b3,1,internal,Exit B3`;
+
---
+++ dictionary.js
@@ -0,0 +1,200 @@
+		// ==========================================
+        // 2. dictionary.js
+        // ==========================================
+        const nodeDict = {
+			"Central & Western District (中西區)": {
+				"Western District (西區)": {
+					"eco_tree_hotel": "Eco Tree Hotel / 頤庭酒店",
+					"hku_campus": "The University of Hong Kong / 香港大學",
+					"hku_campus_c": "HKU Centennial Campus / 香港大學百周年校園"
+				},
+				"Sheung Wan (上環)": {
+					"golden_centre": "Golden Centre / 金龍中心",
+					"harbour_bldg": "Harbour Building / 海港政府大樓",
+					"infinitus_plaza": "Infinitus Plaza / 無限極廣場",
+					"kam_sang_bldg": "Kam Sang Building / 錦甡大廈",
+					"rumsey_carpark": "Rumsey St Carpark / 林士街停車場",
+					"shun_tak_centre": "Shun Tak Centre / 信德中心",
+					"tung_hip_bldg": "Tung Hip Commercial Bldg / 東協商業大廈",
+					"two_chinachem": "Two Chinachem Plaza / 華懋廣場II期",
+					"yue_thai_bldg": "Yue Thai Commercial Bldg / 裕泰商業大廈"
+				},
+				"Hong Kong Station (香港站)": {
+					"100qrc": "100 QRC / 皇后大道中100號",
+					"central_market": "Central Market / 中環街市",
+					"central_pier_3": "Central Pier No. 3 / 中環3號碼頭",
+					"central_pier_7": "Central Pier No. 7 (Star Ferry) / 中環7號碼頭",
+					"central_yards": "Central Yards / 中環新海濱",
+					"1&2_exchange_square": "One & Two Exchange Sq / 交易廣場一及二期",
+					"3_exchange_square": "Three Exchange Sq / 交易廣場三期",
+					"gpo": "General Post Office / 郵政總局",
+					"hang_seng_hq": "Hang Seng HQ / 恒生銀行總行",
+					"ifc_mall": "IFC Mall / 國際金融中心商場",
+					"one_ifc": "One IFC / 國際金融中心一期",
+					"two_ifc": "Two IFC / 國際金融中心二期",
+					"jardine_house": "Jardine House / 怡和大廈",
+					"nexxus_bldg": "Nexxus Building / 盈置大廈"
+				},
+				"Central Core (中環核心)": {
+					"9qrc": "9 Queen's Road Central / 皇后大道中九號",
+					"aia_central": "AIA Central / 友邦金融中心",
+					"alexandra_house": "Alexandra House / 歷山大廈",
+					"bea_bldg": "Bank of East Asia Building / 東亞銀行大廈",
+					"ccb_tower": "CCB Tower / 中國建設銀行大廈",
+					"central_bldg": "Central Building / 中建大廈",
+					"central_tower": "Central Tower / 中匯大廈",
+					"chater_garden": "Chater Garden / 遮打花園",
+					"chater_house": "Chater House / 遮打大廈",
+					"cheung_kong_centre": "Cheung Kong Center / 長江集團中心",
+					"landmark_edinburgh": "Edinburgh Tower / 公爵大廈",
+					"entertainment_bldg": "Entertainment Building / 娛樂行",
+					"landmark_gloucester": "Gloucester Tower / 告羅士打大廈",
+					"hsbc_bldg": "HSBC Main Building / 滙豐總行大廈",
+					"landmark_mall": "The Landmark (Mall) / 置地廣場",
+					"mandarin_oriental": "Mandarin Oriental / 文華東方酒店",
+					"princes_bldg": "Prince's Building / 太子大廈",
+					"scb_bldg": "Standard Chartered Bank / 渣打銀行大廈",
+					"the_henderson": "The Henderson",
+					"the_murray": "The Murray / 美利酒店",
+					"three_garden_road": "Three Garden Road / 花園道三號",
+					"world_wide_house": "World-Wide House / 環球大廈",
+					"landmark_york": "York House / 約克大廈"
+				},
+				"Admiralty (金鐘)": {
+					"admiralty_centre": "Admiralty Centre / 海富中心",
+					"bank_of_america": "Bank of America Tower / 美國銀行中心",
+					"central_gov_offices": "Central Government Offices / 政府總部",
+					"citic_tower": "CITIC Tower / 中信大廈",
+					"fairmont_house": "Fairmont House / 東昌大廈",
+					"far_east_finance": "Far East Finance Centre / 遠東金融中心",
+					"high_court": "High Court / 高等法院",
+					"lippo_centre": "Lippo Centre / 力寶中心",
+					"pacific_place_lg1": "Pacific Place (LG1 - Exit F) / 太古廣場 (地庫一樓)",
+					"pacific_place_l2": "Pacific Place (L2 - Bridges) / 太古廣場 (二樓天橋)",
+					"three_pacific_place": "Three Pacific Place / 太古廣場三座",
+					"queensway_gov_offices": "Queensway Gov Offices / 金鐘道政府合署",
+					"queensway_plaza": "Queensway Plaza / 金鐘廊",
+					"united_centre": "United Centre / 統一中心"
+				}
+			},
+    
+    		"Wan Chai & Causeway Bay (灣仔及銅鑼灣)": {				
+				"Wan Chai (灣仔)": {
+					"capital_ctr": "Capital Centre / 資本中心",
+					"causeway_ctr": "Causeway Centre / 灣景中心大廈",
+					"central_plaza": "Central Plaza / 中環廣場",
+					"china_overseas_bldg": "China Overseas Building / 中國海外大廈",
+					"cr_bldg": "China Resources Building / 華潤大廈",
+					"great_eagle_ctr": "Great Eagle Centre / 鷹君中心",
+					"harbour_ctr": "Harbour Centre / 海港中心",
+					"hrsc": "Harbour Road Sports Centre / 港灣道體育館",
+					"hkcec": "HKCEC / 香港會議展覽中心",
+					"immigration_tower": "Immigration Tower / 入境事務大樓",
+					"lee_tung_avenue": "Lee Tung Avenue / 利東街",
+					"southorn_stadium": "Southorn Stadium / 修頓體育館",
+					"st_regis": "St. Regis Hong Kong / 香港瑞吉酒店",
+					"shk_ctr": "Sun Hung Kai Centre / 新鴻基中心",
+					"wcsp": "Wan Chai Swimming Pool / 灣仔游泳池"
+				},
+				"Causeway Bay (銅鑼灣)": {
+					"cwb_plaza": "Causeway Bay Plaza 1 / 銅鑼灣廣場一期",
+					"hysan_place": "Hysan Place / 希慎廣場",
+					"sogo_cwb": "SOGO / 崇光百貨",
+					"times_square": "Times Square / 時代廣場"
+				}
+			},
+
+    		"Eastern District (東區)": {
+				"Fortress Hill (炮台山)": {
+					"fortress_metro_tower": "Fortress Metro Tower / 康澤花園",
+					"electric_road_msb": "Electric Rd Municipal Services Bldg / 電氣道市政大廈",
+					"harbour_glory": "Harbour Glory / 維港頌"
+				},
+				"North Point (北角)": {
+					"china_united_centre": "China United Centre / 華滙中心",
+					"odeon_building": "Odeon Building / 國賓大廈",
+					"roca_centre": "Roca Centre / 樂嘉中心"
+				},
+				"Tai Koo (太古)": {
+					"1111_kings": "1111 King's Road / 英皇道1111號",
+					"12_taikoo": "12 Taikoo Wan Road / 太古灣道12號",
+					"14_taikoo": "14 Taikoo Wan Road / 太古灣道14號",	            
+					"cityplaza_gf": "Cityplaza G/F / 太古城中心地下",
+					"cityplaza_1f": "Cityplaza 1/F / 太古城中心一樓",
+					"cityplaza_2f": "Cityplaza 2/F / 太古城中心二樓",
+					"east_hk": "East Hong Kong / 東隅",
+					"horizon_gardens": "Horizon Gardens / 海天花園",
+					"kam_din_terrace": "Kam Din Terrace / 金殿臺",
+					"kao_shan_terrace": "Kao Shan Terrace / 高山臺",
+					"kornhill_a-h": "Kornhill Blocks A-H / 康怡花園A-H座",
+					"kornhill_j-m": "Kornhill Blocks J-M / 康怡花園J-M座",
+					"kornhill_plaza_n": "Kornhill Plaza (North) / 康怡廣場(北)",
+					"kornhill_plaza_s": "Kornhill Plaza (South) / 康怡廣場(南)",
+					"sing_fai_terrace": "Sing Fai Terrace / 星輝臺"
+				},
+				"Sai Wan Ho (西灣河)": {
+					"felicity_garden": "Felicity Garden / 欣景花園",
+				},
+				"Chai Wan (柴灣)": {
+					"hing_wah_1_estate": "Hing Wah (I) Estate / 興華(一)邨",
+					"new_jade_garden": "New Jade Garden / 新翠花園",
+					"paradise_mall": "Paradise Mall / 杏花新城",
+					"wan_tsui_estate": "Wan Tsui Estate / 環翠邨",
+					"youth_square": "Youth Square / 青年廣場"
+				}
+			},
+			"MTR Concourses (港鐵大堂)": {
+				"Island Line (港島綫)": {
+					"hku_concourse": "HKU / 香港大學站",
+					"syp_concourse": "Sai Ying Pun / 西營盤站",
+					"sheung_wan_concourse_mid": "Sheung Wan (Exits A-D) / 上環站 (A-D出口)",
+					"sheung_wan_concourse_east": "Sheung Wan (Exit E) / 上環站 (E出口)",
+					"hong_kong_concourse": "Hong Kong / 香港站",
+					"central_concourse_west": "Central (Exits A-E) / 中環站 (A-E出口)",
+					"central_concourse_mid": "Central (Exits F-H) / 中環站 (F-H出口)",
+					"central_concourse_east": "Central (Exits J-L) / 中環站 (J-L出口)",
+					"admiralty_concourse": "Admiralty / 金鐘站",
+					"wan_chai_concourse": "Wan Chai / 灣仔站",
+					"causeway_bay_concourse_a": "Causeway Bay (Exit A) / 銅鑼灣站 (A出口)",
+					"causeway_bay_concourse_bc": "Causeway Bay (Exits B-C) / 銅鑼灣站 (B-C出口)",
+					"causeway_bay_concourse_df": "Causeway Bay (Exits D-F) / 銅鑼灣站 (D-F出口)",
+					"fortress_hill_concourse": "Fortress Hill / 炮台山站",
+					"north_point_concourse": "North Point / 北角站",
+					"tai_koo_concourse": "Tai Koo / 太古站",
+					"sai_wan_ho_concourse": "Sai Wan Ho / 西灣河站",
+					"heng_fa_chuen_concourse": "Heng Fa Chuen / 杏花邨站",
+					"chai_wan_concourse": "Chai Wan / 柴灣站",
+				},
+				"East Rail Line (東鐵綫)": {
+					"admiralty_concourse": "Admiralty / 金鐘站",
+					"exhibition_centre_concourse": "Exhibition Centre / 會展站"
+				}
+			},
+			"MTR Platforms (付費區)": {
+				"Island Line (港島綫)": {
+					"kennedy_town_platform": "Kennedy Town / 堅尼地城站",
+					"hku_platform": "HKU / 香港大學站",
+					"sai_ying_pun_platform": "Sai Ying Pun / 西營盤站",
+					"sheung_wan_platform": "Sheung Wan / 上環站",
+					"central_platform_island": "Central / 中環站",
+					"hong_kong_platform": "Hong Kong / 香港站",
+					"admiralty_platform": "Admiralty (Island and Tsuen Wan Lines) / 金鐘站(港島綫及荃灣綫)",
+					"wan_chai_platform": "Wan Chai / 灣仔站",
+					"causeway_bay_platform": "Causeway Bay / 銅鑼灣站",
+					"tin_hau_platform": "Tin Hau / 天后站",
+					"fortress_hill_platform": "Fortress Hill / 炮台山站",
+					"north_point_platform": "North Point / 北角站",
+					"quarry_bay_platform": "Quarry Bay / 鰂魚涌站",
+					"tai_koo_platform": "Tai Koo / 太古站",
+					"sai_wan_ho_platform": "Sai Wan Ho / 西灣河站",
+					"shau_kei_wan_platform": "Shau Kei Wan / 筲箕灣站",
+					"heng_fa_chuen_platform": "Heng Fa Chuen / 杏花邨站",
+					"chai_wan_platform": "Chai Wan / 柴灣站",
+				},
+				"East Rail Line (東鐵綫)": {
+					"admiralty_platform_erl": "Admiralty (East Rail) / 金鐘站(東鐵綫)",
+					"exhibition_centre_platform": "Exhibition Centre / 會展站"
+				}
+			}
+        };
+
