992bd76598b12e33a81e9032a215f44a0e77f0d3
Ronnie <tseronnie@ymail.com>
2026-05-29 12:57:38 +0800

    generic "Platform Transfer" for Nam Cheong, to align with Tai Wai

--- data.js
+++ data.js
@@ -598,7 +598,7 @@ ma_on_shan_platform,wu_kai_sha_platform,5,paid_train,Tuen Ma Line (MTR)
 
 // --- TUEN MA LINE TRANSFERS ---
 mei_foo_platform_tml,mei_foo_platform_twl,15,paid_tunnel,Pedestrian Subway to Tsuen Wan Line
-nam_cheong_platform_tml,nam_cheong_platform_tcl,4,paid_transfer,Vertical Concourse Transfer
+nam_cheong_platform_tml,nam_cheong_platform_tcl,4,paid_transfer,Platform Transfer
 east_tst_platform,tsim_sha_tsui_platform,18,paid_tunnel,Subway to Tsuen Wan Line
 hung_hom_platform_tml,hung_hom_platform_erl,4,paid_transfer,Escalator Transfer
 ho_man_tin_platform_tml,ho_man_tin_platform_ktl,8,paid_transfer,Escalator Transfer
